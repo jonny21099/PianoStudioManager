3dbdf5c43906c863b3b9584ac62034d6932be891
jonny21099 <jcmartin@ualberta.ca>
2020-06-14 22:53:35 -0600

    Updated database with update date

---
+++ PianoProgram/Add_student.py
@@ -0,0 +1,21 @@
+import os
+from datetime import date
+	
+def Add_student(c, conn):
+	student_name = input("What is the student's name?\n")
+	lesson_day = input("On what days do you teach this student?\n")
+	lesson_time = input("At what time do you teach this student?\n")
+	lesson_length = input("How long is each lesson?\n")
+	lesson_price = input("How much are you charging each lesson?\n")
+	os.system("clear")
+	user_response = input("Are you sure you are satisfied with all the entries(y/n)?\nname = %s\nday = %s\ntime = %s\nlength = %s\nprice = %s\n" % (student_name, lesson_day, lesson_time, lesson_length, lesson_price))
+	if user_response.lower() == "y":
+		today = date.today()
+		today_date = today.strftime("%m/%d/%Y")
+		print("Adding student to database")
+		c.execute("INSERT INTO calendar VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (student_name, lesson_day, lesson_time, lesson_length, lesson_price, 0, 0, today_date))
+		conn.commit()
+		#Add student to database
+	elif user_response.lower() == "n":
+		os.system("clear")
+		Add_student()
--- PianoProgram/Calendar.py
+++ PianoProgram/Calendar.py
@@ -1,12 +1,14 @@
 import sqlite3
 import os
 import time
+from PianoProgram.Add_student import Add_student
+
 
 conn = sqlite3.connect("teaching.db")
 c = conn.cursor()
 
 def Calendar():
-	user_option = input("What would you like to do?\n1.View all unpaid lessons\n2.View all lessons\n3.Add new student\n")
+	user_option = input("What would you like to do?\n1.View all unpaid lessons\n2.Update \n3.Add new student\n")
 	#View all unpaid lessons
 	if user_option == "1":
 		print("1")
@@ -14,12 +16,13 @@ def Calendar():
 	#View all lessons
 	elif user_option == "2":
 		print("1")
-		# View_all()
+		# Update()
+		
 	#Add new students
 	elif user_option == "3":
 		time.sleep(0.5)
 		os.system("clear")
-		Add_student()
+		Add_student(c, conn)
 
 	#Invalid entry
 	else:
@@ -29,26 +32,3 @@ def Calendar():
 		Calendar()
 
 
-# def View_unpaid():
-#	#view all unpaid lessons
-
-# def View_all():
-# 	#View all lessons 
-
-	
-def Add_student():
-	student_name = input("What is the student's name?\n")
-	lesson_day = input("On what days do you teach this student?\n")
-	lesson_time = input("At what time do you teach this student?\n")
-	lesson_length = input("How long is each lesson?\n")
-	lesson_price = input("How much are you charging each lesson?\n")
-	os.system("clear")
-	user_response = input("Are you sure you are satisfied with all the entries(y/n)?\nname = %s\nday = %s\ntime = %s\nlength = %s\nprice = %s\n" % (student_name, lesson_day, lesson_time, lesson_length, lesson_price))
-	if user_response.lower() == "y":
-		print("Adding student to database")
-		c.execute("INSERT INTO calendar VALUES (?, ?, ?, ?, ?, ?, ?)", (student_name, lesson_day, lesson_time, lesson_length, lesson_price, 0, 0))
-		conn.commit()
-		#Add student to database
-	elif user_response.lower() == "n":
-		os.system("clear")
-		Add_student()
